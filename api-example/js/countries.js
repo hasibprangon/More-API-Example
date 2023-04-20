889501db28117ee1944eea512fbabf1a2233ca10
hasibprangon <hasibulhossainp09@gmail.com>
2023-04-20 14:17:53 +0600

    displaying flag and details dynamically

--- api-example/js/countries.js
+++ api-example/js/countries.js
@@ -11,16 +11,37 @@ const displayCountries = countries => {
     // }
     // console.log(countries)
     countries.forEach(country => {
-        console.log(country);
+        // console.log(country.cca2);
 
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country');
         countryDiv.innerHTML = `
         <h3>Name: ${country.name.common}</h3>
         <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+        <button onclick="loadCuontryDetailes('${country.cca2}')"> Details </button>
         `;
         countriesContainer.appendChild(countryDiv)
     });
 
 }
+
+const loadCuontryDetailes = code => {
+    // https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}
+    `
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCuontryDetailes(data[0]))
+}
+
+const displayCuontryDetailes = country => {
+    console.log(country)
+    const detailContainer = document.getElementById('country-details');
+    detailContainer.innerHTML = `
+    <img src="${country.flags.png}">
+<h3> Name: ${country.name.common}</h3>
+    `
+}
+
 loadCountries();
